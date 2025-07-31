bcac86cadaea58d38bfe31e6eb3e6a5b813dc2a7
Rakhmiel <ryandobron@gmail.com>
2025-07-31 12:37:32 -0400

    Initial Commit

---
+++ ClipFriend/ClipFriendApp.swift
@@ -0,0 +1,17 @@
+//
+//  ClipFriendApp.swift
+//  ClipFriend
+//
+//  Created by Ryan Dobron on 7/31/25.
+//
+
+import SwiftUI
+
+@main
+struct ClipFriendApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
